e9713ad0115dfdfe04778d3a6a21b515bd99f5d3
marek-sroczkowski <marek.sroczkowski.dev@gmail.com>
2022-12-09 03:01:51 +0100

    Add project files.

---
+++ LabAuthorizationTS/Authorization/Handlers/MinimumAgeHandler.cs
@@ -0,0 +1,22 @@
+﻿using LabAuthorizationTS.Authorization.Requirements;
+using LabAuthorizationTS.Extensions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LabAuthorizationTS.Authorization.Handlers
+{
+    public class MinimumAgeHandler : AuthorizationHandler<MinimumAgeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAgeRequirement requirement)
+        {
+            //TODO sprawdzenie czy użytkownik jest pełnoletni (metoda rozszerzająca IsAdult)
+
+            var dateOfBirth = context.User.GetUserBirthDate();
+            if (dateOfBirth.HasValue && dateOfBirth.Value.IsAdult(requirement.MinimumAge))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ LabAuthorizationTS/Authorization/Handlers/ProductResourceOperationHandler.cs
@@ -0,0 +1,50 @@
+﻿using LabAuthorizationTS.Authorization.Requirements;
+using LabAuthorizationTS.Authorization.Utils;
+using LabAuthorizationTS.Extensions;
+using LabAuthorizationTS.Models.Dtos.Products;
+using LabAuthorizationTS.Models.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LabAuthorizationTS.Authorization.Handlers
+{
+    public class ProductResourceOperationHandler : AuthorizationHandler<ResourceOperationRequirement, ProductDto>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationRequirement requirement, ProductDto resource)
+        {
+            if (requirement.OperationType == OperationType.Create)
+            {
+                context.Succeed(requirement);
+            }
+
+            //TODO Admin może wszystko, jeśli produkt nie ma ograniczenia wiekowego, a użytkownik to Customer to Succeed
+            //jeśli produkt ma ograniczenie wiekowe, a użytkownik to pełnoletni Customer to Succeed
+            //jeśli użytkownik to Seller, i jest to jego produkt to Succeed
+
+            var role = context.User.GetUserRole();
+            if (role == UserRole.Admin.ToString())
+            {
+                context.Succeed(requirement);
+            }
+
+            if (requirement.OperationType == OperationType.Read && !resource.OnlyForAdults)
+            {
+                context.Succeed(requirement);
+            }
+
+            var dateOfBirth = context.User.GetUserBirthDate();
+            if (requirement.OperationType == OperationType.Read && resource.OnlyForAdults
+                && dateOfBirth.HasValue && dateOfBirth.Value.IsAdult(18))
+            {
+                context.Succeed(requirement);
+            }
+
+            var userId = context.User.GetUserId();
+            if (role == UserRole.Seller.ToString() && resource.UserId == userId)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
